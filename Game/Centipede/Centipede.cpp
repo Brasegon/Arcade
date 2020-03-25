18f9556f13e45e946e2b70edcdcfccfcb9da2077
Brandon Segers <unconfigured@null.spigotmc.org>
2020-03-25 11:27:58 +0100

    Début du centipede

---
+++ Game/Centipede/Centipede.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** Arcade [WSL: Debian]
+** File description:
+** Centipede
+*/
+
+#include "Centipede.hpp"
+
+Centipede::Centipede()
+{
+    
+}
+
+Centipede::~Centipede()
+{
+}
--- Game/IDisplayModule.hpp
+++ Game/IDisplayModule.hpp
@@ -9,11 +9,12 @@
 #define IDISPLAYMODULE_HPP_
 
 #include <string>
-
+#include <vector>
 class IDisplayModule {
     virtual ~IDisplayModule() = default;
     virtual void init() = 0;
     virtual void stop() = 0;
+    virtual void update() = 0;
     virtual const std::string &getName() const = 0;
 
     protected:
