6956fc0e1a036f7b3944b93f2fe75f97371cf2fb
bterras <baptiste.terras@epitech.eu>
2020-03-31 19:06:57 +0200

    restoration du core comme avant

--- src/Core/Core.cpp
+++ src/Core/Core.cpp
@@ -133,6 +133,11 @@ void Core::event(playerEvent record_key)
     //     restartArcade();
 }
 
+void Core::drawGame_Map()
+{
+
+}
+
 void Core::nextGame_Lib()
 {
     int max_game_lib = gameList.size() - 1;
